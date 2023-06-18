7ae621528acdc3798b7da703616566053bfd2975
NguyenGiaHy123 <89922176+NguyenGiaHy123@users.noreply.github.com>
2023-06-18 23:12:00 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import React from 'react';
+
 import logo from './logo.svg';
+import React, { useState ,Suspense} from 'react';
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+
+
+import NotFound from './component/RouterDom/NotFound';
+import AllPage from '../src/component/RouterDom/Page'
+import Loading from './Looading';
+import "swiper/css";
+import "swiper/css/effect-flip";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 
 function App() {
+  const ShowAllPage=(AllPage:any)=>{
+    if(AllPage.length>0){
+      return AllPage.map((item:any,index:number)=>{
+        return <Route
+         key={index} 
+        path={item.path}
+         element={<Suspense fallback={<Loading/> }>
+            {item.main}
+         </Suspense>}/>
+      })
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='app' >
+        <Routes>
+          {ShowAllPage(AllPage)}
+          <Route path='/*' element={<NotFound/>}/>
+        </Routes>
+      
+      </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom'
 
+  import { ChatContextProvider } from './component/Context/ContextChat';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+
+  <BrowserRouter>
+    <React.StrictMode>
+    <ChatContextProvider>
     <App />
+    </ChatContextProvider>
   </React.StrictMode>
+  </BrowserRouter>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
